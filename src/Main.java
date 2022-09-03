d7f1daa2ecf06b686d77368835ae0489fa0ca65f
ruslan <ruslan.sharipov.zokirovich@yandex.ru>
2022-09-03 22:09:29 +0500

    Complite program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        List<Integer> array = new ArrayList<>();
+        array.add(1);
+        array.add(2);
+        array.add(5);
+        array.add(16);
+        array.add(-1);
+        array.add(-2);
+        array.add(0);
+        array.add(32);
+        array.add(3);
+        array.add(5);
+        array.add(8);
+        array.add(23);
+        array.add(4);
+
+        Collections.sort(array);
+
+        for (int arrays : array) {
+            if (arrays > 0 && arrays % 2 == 0) {
+                System.out.print("[" + arrays  + "]");
+            }
+        }
+
+        System.out.println();
+
+        StreamMain.main();
     }
 }
--- src/StreamMain.java
+++ src/StreamMain.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class StreamMain {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamMain {
+   public static void main() {
+       List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+       list.stream()
+               .filter(x -> x > 0)
+               .filter(x -> x % 2 == 0)
+               .sorted(Comparator.naturalOrder())
+               .forEach(System.out::print);
+   }
+
+
+
+
 }
